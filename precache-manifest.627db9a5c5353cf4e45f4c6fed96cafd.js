b24c4bddaf0dc0cd6ae795d7edb5c425697fd551
barriem <barriem@wustl.edu>
2018-12-14 13:46:07 -0600

    Updates

---
+++ precache-manifest.627db9a5c5353cf4e45f4c6fed96cafd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/09-final-project-barrie-avila/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "793a6ed8958ee1666f97",
+    "url": "/09-final-project-barrie-avila/static/css/main.010387f3.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/09-final-project-barrie-avila/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "3dcf783bc818465cfa6e",
+    "url": "/09-final-project-barrie-avila/static/js/1.3dcf783b.chunk.js"
+  },
+  {
+    "revision": "269cf5a39a53801d52d5",
+    "url": "/09-final-project-barrie-avila/static/js/runtime~main.269cf5a3.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/09-final-project-barrie-avila/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "793a6ed8958ee1666f97",
+    "url": "/09-final-project-barrie-avila/static/js/main.793a6ed8.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/09-final-project-barrie-avila/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/09-final-project-barrie-avila/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/09-final-project-barrie-avila/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "3dcf783bc818465cfa6e",
+    "url": "/09-final-project-barrie-avila/static/css/1.fe832650.chunk.css"
+  },
+  {
+    "revision": "e17d231f0afab80baf7d86dabd853c15",
+    "url": "/09-final-project-barrie-avila/index.html"
+  }
+];
